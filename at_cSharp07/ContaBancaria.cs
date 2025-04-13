8104829d33d9c8d6d952627d2bf477f6b6e1f0f0
Isabella Mayer <bella@MacBook-Pro-de-Isabella.local>
2025-04-13 16:07:11 -0300

    conta bancaria

---
+++ at_cSharp07/ContaBancaria.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class ContaBancaria
+{
+    public string Titular { get; set; }
+    private decimal saldo;
+
+    public ContaBancaria(string titular)
+    {
+        Titular = titular;
+        saldo = 0;
+    }
+
+    public void Depositar(decimal valor)
+    {
+        if (valor > 0)
+        {
+            saldo += valor;
+            Console.WriteLine($"Depósito de R$ {valor:F2} realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("O valor do depósito deve ser positivo!");
+        }
+    }
+
+    public void Sacar(decimal valor)
+    {
+        if (valor > saldo)
+        {
+            Console.WriteLine("Saldo insuficiente");
+        }
+        else if (valor > 0)
+        {
+            saldo -= valor;
+            Console.WriteLine($"Saque de R$ {valor:F2} realizado com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("O valor do saque tem qu ser positivo!");
+        }
+    }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Saldo atual: R$ " + saldo.ToString("F2"));
+    }
+}
---
+++ at_cSharp07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ContaBancaria conta = new ContaBancaria("belmayer");
+        
+        Console.WriteLine($"Titular: {conta.Titular}\n");
+
+        conta.Depositar(300);
+        conta.MostrarInfo();
+
+        Console.WriteLine("\nsaque: R$ 700,00");
+        conta.Sacar(500);
+
+        conta.Sacar(200);
+        conta.MostrarInfo();
+    }
+}
